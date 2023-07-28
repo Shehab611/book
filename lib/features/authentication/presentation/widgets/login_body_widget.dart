97f5a5f18fba10cbcda655b4446d08cc00badf3c
shehab <shehabehab1029@gmail.com>
2023-07-29 01:25:17 +0300

    finish all login screen logic

--- lib/features/authentication/presentation/widgets/login_body_widget.dart
+++ lib/features/authentication/presentation/widgets/login_body_widget.dart
@@ -2,7 +2,6 @@ import 'package:book/constants.dart';
 import 'package:book/core/utils/app_router.dart';
 import 'package:book/core/utils/services_locator.dart';
 import 'package:book/features/authentication/data/repositories/login/login_repo_impl.dart';
-import 'package:book/features/authentication/data/repositories/reset_password/reset_password_repo_impl.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../components/go_in_button.dart';
