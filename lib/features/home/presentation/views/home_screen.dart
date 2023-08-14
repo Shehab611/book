e3241c1ba9df4637c5bd4e8ef02bab594841f479
shehab <shehabehab1029@gmail.com>
2023-08-14 07:28:44 +0300

    refactor home page design

--- lib/features/home/presentation/views/home_screen.dart
+++ lib/features/home/presentation/views/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:book/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'widgets/home_body_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,10 +14,19 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: kDefaultColor.withOpacity(.2),
         elevation: 0,
-        title: const Text('Home Page'),
+        title:Text(
+          'Home',
+          style: GoogleFonts.montserrat(
+              color: kColor, fontWeight: FontWeight.w800)
+              .copyWith(
+              fontSize: Theme.of(context)
+                  .textTheme
+                  .headlineSmall
+                  ?.fontSize),
+        ),
         centerTitle: true,
         actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search,size: 30,color: kColor,)),
         ],
       ),
       body:const HomeBodyWidget(),
